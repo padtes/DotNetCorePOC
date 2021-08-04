dd6848d4099641c24b1cd130f60749189f7e74d8
padtes <padtes@gmail.com>
2021-08-03 20:29:31 -0500

    string format draft

--- DataProcessor/CommandHandler.cs
+++ DataProcessor/CommandHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace DataProcessor
 {
@@ -39,6 +40,9 @@ namespace DataProcessor
             if (commandName.Equals("DateFormat", StringComparison.OrdinalIgnoreCase))
                 return new DateFormatter();
 
+            if (commandName.Equals("StrFormat", StringComparison.OrdinalIgnoreCase))
+                return new StrFormatter();
+
             if (commandName.Equals("RowCount", StringComparison.OrdinalIgnoreCase))
                 return new RowCount();
 
@@ -74,7 +78,98 @@ namespace DataProcessor
         }
     }
 
+    public class StrFormatter : Command
+    {
+        //param 0 - data
+        //param 1 - format: no_fmt, toupper, tolower, totile
+        //param 2 - no_trim, trimstart, trimend, trim, singlespace
+        public override string Run(string[] pArr, string[] progParams, DataRow dr, out bool isConst)
+        {
+            isConst = false;
+            if (pArr == null || pArr.Length < 2)
+                throw new Exception("invalid parameters for dateformat value");
 
+            int argStInd = pArr[0].IndexOf("[");
+            int argEndInd = pArr[0].LastIndexOf("]");
+            if (argStInd < 1 || argStInd > argEndInd)
+            {
+                throw new Exception("malformed string param:" + pArr[0]);
+            }
+
+            string src = pArr[0].Substring(0, argStInd).Trim();
+            string valIndxStr = pArr[0].Substring(argStInd + 1, (argEndInd - argStInd - 1));
+            string valAsStr;
+            try
+            {
+                if (src.ToLower() == "args")
+                {
+                    int valIndx = int.Parse(valIndxStr);
+                    valAsStr = progParams[valIndx];
+                    isConst = true;
+                }
+                else
+                if (src.ToLower() == "dr")
+                {
+                    int valIndx;
+                    if (int.TryParse(valIndxStr, out valIndx))
+                    {
+                        valAsStr = Convert.ToString(dr[valIndx]);
+                    }
+                    else
+                    {
+                        valAsStr = Convert.ToString(dr[valIndxStr]);
+                    }
+                }
+                else
+                {
+                    throw new Exception("invalid parameter for dateformat value " + pArr[0] + " use dr or args");
+                }
+            }
+            catch (Exception ed)
+            {
+                Exception e1 = new Exception(ed.Message + " invalid date " + pArr[0], ed);
+                throw e1;
+            }
+            string trimWhat = "no_trim";
+            if (pArr.Length > 2)
+            {
+                trimWhat = pArr[2].ToLower();
+            }
+            switch (trimWhat)
+            {
+                case "trim":
+                    valAsStr = valAsStr.Trim();
+                    break;
+                case "trimstart":
+                    valAsStr = valAsStr.TrimStart();
+                    break;
+                case "trimend":
+                    valAsStr = valAsStr.TrimEnd();
+                    break;
+                case "singlespace":
+                    valAsStr = Regex.Replace(valAsStr, "[ ]{2,}", " ", RegexOptions.None);
+                    break;
+                default:
+                    break;
+            }
+            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+            switch (pArr[1])
+            {
+                case "toupper":
+                    valAsStr = ti.ToUpper(valAsStr);
+                    break;
+                case "tolower":
+                    valAsStr = ti.ToLower(valAsStr);
+                    break;
+                case "totile":
+                    valAsStr = ti.ToTitleCase(valAsStr);
+                    break;
+                default:
+                    break;
+            }
+            return valAsStr;
+        }
+    }
     public class DateFormatter : Command
     {
         //param 0 - data  - now
