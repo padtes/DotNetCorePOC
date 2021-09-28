1a475ea8740a2200d58bd13eaadeea60862bc7a7
padtes <padtes@gmail.com>
2021-09-27 21:11:37 -0500

    Enhanced EscapeJson

--- DataProcessor/InputRecordAbs.cs
+++ DataProcessor/InputRecordAbs.cs
@@ -121,7 +121,62 @@ namespace DataProcessor
 
         public static string EscapeJson(string val)
         {
-            return val.Replace("\"", "\\\"");
+            //return val.Replace("\"", "\\\"");
+
+            if (val == null || val.Length == 0)
+            {
+                return "";
+            }
+
+            char c = '\0';
+            int i;
+            int len = val.Length;
+            StringBuilder sb = new StringBuilder(len + 4);
+            String t;
+
+            for (i = 0; i < len; i += 1)
+            {
+                c = val[i];
+                switch (c)
+                {
+                    case '\\':
+                    case '"':
+                        sb.Append('\\');
+                        sb.Append(c);
+                        break;
+                    case '/':
+                        sb.Append('\\');
+                        sb.Append(c);
+                        break;
+                    case '\b':
+                        sb.Append("\\b");
+                        break;
+                    case '\t':
+                        sb.Append("\\t");
+                        break;
+                    case '\n':
+                        sb.Append("\\n");
+                        break;
+                    case '\f':
+                        sb.Append("\\f");
+                        break;
+                    case '\r':
+                        sb.Append("\\r");
+                        break;
+                    default:
+                        if (c < ' ')
+                        {
+                            t = "000" + String.Format("X", c);
+                            sb.Append("\\u" + t.Substring(t.Length - 4));
+                        }
+                        else
+                        {
+                            sb.Append(c);
+                        }
+                        break;
+                }
+            }
+            return sb.ToString();
         }
     }
     public class JsonArrOfColsWithVals
